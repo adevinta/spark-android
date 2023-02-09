3c9eb36246618134e92365ec713eedb0588004c5
Simon Marquis <simon.marquis@adevinta.com>
2023-02-09 12:53:04 +0100

    fix: remove unnecessary testInstrumentationRunner

--- spark/build.gradle.kts
+++ spark/build.gradle.kts
@@ -14,7 +14,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFile("consumer-rules.pro")
     }
 
