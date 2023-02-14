fd3ee725333f06d155875a2750be4b9059d598a4
Scott Rayapoullé <scott.rayapoulle.ext@adevinta.com>
2023-02-14 17:41:54 +0100

    Sort dependencies alphabeticaly
    Co-authored-by: Simon Marquis <contact@simon-marquis.fr>

--- spark/build.gradle.kts
+++ spark/build.gradle.kts
@@ -128,8 +128,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
