4fdbfcf5b9405d8fe1b417787e6935dbf93385fc
Simon Marquis <simon.marquis@adevinta.com>
2023-02-16 14:54:33 +0100

    chore: cleanup sample app by removing unnecessary code

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -65,14 +65,4 @@ android {
 
 dependencies {
     implementation(projects.spark)
-
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.testManifest)
 }
