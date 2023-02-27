064315adbb09d1b2a8397152c3b40389f5304a49
Scott Rayapoullé <scott.rayapoulle.ext@adevinta.com>
2023-02-27 20:36:49 +0100

    Add Rating

---
+++ spark/src/main/kotlin/com/adevinta/spark/components/rating/RatingStar.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2023 Adevinta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.adevinta.spark.components.rating
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import com.adevinta.spark.InternalSparkApi
+import com.adevinta.spark.PreviewTheme
+import com.adevinta.spark.SparkTheme
+import com.adevinta.spark.components.icons.Icon
+import com.adevinta.spark.icons.SparkIcon
+import com.adevinta.spark.tokens.DisabledAlpha
+import com.adevinta.spark.tools.preview.SparkPreviewParam
+import com.adevinta.spark.tools.preview.SparkPreviewParamProvider
+
+/**
+ * RatingStar is the atomic element of rating components
+ * @param modifier to be applied
+ * @param enabled whether the star should be colored
+ */
+
+const val BRIKKE_RATING_STAR_SIZE = 12
+
+@Composable
+@InternalSparkApi
+internal fun SparkRatingStar(
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    val color by animateColorAsState(
+        targetValue = if (enabled) SparkTheme.colors.primary else SparkTheme.colors.onSurface.copy(
+            DisabledAlpha,
+        ),
+    )
+
+    CompositionLocalProvider(LocalContentColor provides color) {
+        Icon(
+            modifier = modifier.size(BRIKKE_RATING_STAR_SIZE.dp),
+            sparkIcon = SparkIcon.Options.Star.Default,
+            contentDescription = null,
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun RatingStarPreview(
+    @PreviewParameter(SparkPreviewParamProvider::class) param: SparkPreviewParam,
+) {
+    val (theme, userType) = param
+    PreviewTheme(theme, userType) {
+        SparkRatingStar(enabled = true)
+        SparkRatingStar(enabled = false)
+    }
+}
