23ce8f48a99c7fd684195406ce2697d6c273721a
Scott Rayapoullé <scott.rayapoulle.ext@adevinta.com>
2023-03-01 12:28:17 +0100

    chore: Add tags components (#155)
    * Add Tags
    
    * Update screenshots & remove chip reference in doc
    
    * Fix lint error
    
    * Update explicit api
    
    * Remove unwanted `DisableSelection`

---
+++ spark/src/main/kotlin/com/adevinta/spark/components/tags/TagFilled.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 Adevinta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.adevinta.spark.components.tags
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.adevinta.spark.ExperimentalSparkApi
+import com.adevinta.spark.PreviewTheme
+import com.adevinta.spark.tools.preview.SparkPreviewProvider
+import com.adevinta.spark.tools.preview.ThemeVariant
+import com.adevinta.spark.tools.preview.UserType
+
+/**
+ * Filled tag can be used to identify your brand DNA inside a list.
+ * @param modifier The [Modifier] to be applied to the component
+ * @param content The item label
+ */
+@ExperimentalSparkApi
+@Composable
+public fun TagFilled(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit,
+) {
+    SparkTag(
+        colors = TagDefaults.filledColors(),
+        modifier = modifier,
+        leadingIcon = null,
+        content = content,
+    )
+}
+
+@Preview(
+    group = "Tags",
+)
+@Composable
+internal fun TagFilledPreview(
+    @PreviewParameter(SparkPreviewProvider::class) param: Pair<ThemeVariant, UserType>,
+) {
+    val (theme, userType) = param
+    PreviewTheme(theme, userType) {
+        TagFilled {
+            Text(text = "À la une")
+        }
+    }
+}
