7d211cf7c077f0e46cf012656089a6989afcef52
Simon Marquis <simon.marquis@adevinta.com>
2023-03-02 15:58:58 +0000

    chore: android documentation plugin must follow dokka releases (#216)

--- build-logic/src/main/kotlin/com/adevinta/spark/SparkProperties.kt
+++ build-logic/src/main/kotlin/com/adevinta/spark/SparkProperties.kt
@@ -56,7 +56,7 @@ internal class SparkVersions(catalog: VersionCatalog) {
 @Suppress("HasPlatformType", "PropertyName")
 internal class SparkLibraries(catalog: VersionCatalog) {
     val `androidx-compose-bom` by catalog
-    val `android-documentation-plugin` by catalog
+    val `dokka-android-documentation-plugin` by catalog
     val `kotlin-bom` by catalog
 
     private operator fun VersionCatalog.getValue(
