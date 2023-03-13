77a40f7acbadb8849ea7206d85d792627eea4972
Marina Romanova <marina.romanova@adevinta.com>
2023-03-13 16:44:22 +0100

    chore: Add new typo tokens

--- spark/src/main/kotlin/com/adevinta/spark/tokens/Type.kt
+++ spark/src/main/kotlin/com/adevinta/spark/tokens/Type.kt
@@ -38,6 +38,11 @@ import com.adevinta.spark.SparkTheme
 import com.adevinta.spark.tools.preview.ThemeProvider
 import com.adevinta.spark.tools.preview.ThemeVariant
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("display3"),
+    level = DeprecationLevel.WARNING,
+)
 internal val tile1Type = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 24.sp,
@@ -46,6 +51,11 @@ internal val tile1Type = TextStyle(
     letterSpacing = 0.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("headline"),
+    level = DeprecationLevel.WARNING,
+)
 internal val title2Type = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 20.sp,
@@ -54,6 +64,11 @@ internal val title2Type = TextStyle(
     letterSpacing = 0.15.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("headline2"),
+    level = DeprecationLevel.WARNING,
+)
 internal val title3Type = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 18.sp,
@@ -62,6 +77,11 @@ internal val title3Type = TextStyle(
     letterSpacing = 0.15.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("body1Highlight"),
+    level = DeprecationLevel.WARNING,
+)
 internal val largeImportantType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 16.sp,
@@ -70,6 +90,11 @@ internal val largeImportantType = TextStyle(
     letterSpacing = 0.15.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("body1"),
+    level = DeprecationLevel.WARNING,
+)
 internal val largeType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 16.sp,
@@ -78,6 +103,11 @@ internal val largeType = TextStyle(
     letterSpacing = 0.5.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("body2Highlight"),
+    level = DeprecationLevel.WARNING,
+)
 internal val bodyImportantType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 14.sp,
@@ -86,6 +116,11 @@ internal val bodyImportantType = TextStyle(
     letterSpacing = 0.1.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("body2"),
+    level = DeprecationLevel.WARNING,
+)
 internal val bodyType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 14.sp,
@@ -94,6 +129,11 @@ internal val bodyType = TextStyle(
     letterSpacing = 0.25.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("smallHighlight"),
+    level = DeprecationLevel.WARNING,
+)
 internal val smallImportantType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 12.sp,
@@ -108,6 +148,11 @@ internal val smallType = TextStyle(
     lineHeight = 16.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("captionHighlight"),
+    level = DeprecationLevel.WARNING,
+)
 internal val extraSmallImportantType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 10.sp,
@@ -115,6 +160,11 @@ internal val extraSmallImportantType = TextStyle(
     lineHeight = 10.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("caption"),
+    level = DeprecationLevel.WARNING,
+)
 internal val extraSmallType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 10.sp,
@@ -122,6 +172,11 @@ internal val extraSmallType = TextStyle(
     lineHeight = 10.sp,
 )
 
+@Deprecated(
+    "This property will be removed as it is not part of Spark Token",
+    replaceWith = ReplaceWith("callout"),
+    level = DeprecationLevel.WARNING,
+)
 internal val buttonType = TextStyle(
     fontFamily = FontFamily.Default,
     fontSize = 14.sp,
@@ -131,19 +186,44 @@ internal val buttonType = TextStyle(
 
 public fun sparkTypography(
     fontFamily: FontFamily = FontFamily.Default,
-    title1: TextStyle = tile1Type.copy(fontFamily = fontFamily),
-    title2: TextStyle = title2Type.copy(fontFamily = fontFamily),
-    title3: TextStyle = title3Type.copy(fontFamily = fontFamily),
-    largeImportant: TextStyle = largeImportantType.copy(fontFamily = fontFamily),
-    large: TextStyle = largeType.copy(fontFamily = fontFamily),
-    bodyImportant: TextStyle = bodyImportantType.copy(fontFamily = fontFamily),
-    body: TextStyle = bodyType.copy(fontFamily = fontFamily),
-    smallImportant: TextStyle = smallImportantType.copy(fontFamily = fontFamily),
+    display1: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    display2: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    display3: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    headline1: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    headline2: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    subhead: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    body1: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    body1Highlight: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    body2: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    body2Highlight: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    caption: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    captionHighlight: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    /* deprecated*/ title1: TextStyle = tile1Type.copy(fontFamily = fontFamily),
+    /* deprecated*/ title2: TextStyle = title2Type.copy(fontFamily = fontFamily),
+    /* deprecated*/ title3: TextStyle = title3Type.copy(fontFamily = fontFamily),
+    /* deprecated*/ largeImportant: TextStyle = largeImportantType.copy(fontFamily = fontFamily),
+    /* deprecated*/ large: TextStyle = largeType.copy(fontFamily = fontFamily),
+    /* deprecated*/ bodyImportant: TextStyle = bodyImportantType.copy(fontFamily = fontFamily),
+    /* deprecated*/ body: TextStyle = bodyType.copy(fontFamily = fontFamily),
+    /* deprecated*/ smallImportant: TextStyle = smallImportantType.copy(fontFamily = fontFamily),
     small: TextStyle = smallType.copy(fontFamily = fontFamily),
-    extraSmallImportant: TextStyle = extraSmallImportantType.copy(fontFamily = fontFamily),
-    extraSmall: TextStyle = extraSmallType.copy(fontFamily = fontFamily),
-    button: TextStyle = buttonType.copy(fontFamily = fontFamily),
+    smallHighlight: TextStyle = TextStyle.Default.copy(fontFamily = fontFamily),
+    /* deprecated*/ extraSmallImportant: TextStyle = extraSmallImportantType.copy(fontFamily = fontFamily),
+    /* deprecated*/ extraSmall: TextStyle = extraSmallType.copy(fontFamily = fontFamily),
+    /* deprecated*/ button: TextStyle = buttonType.copy(fontFamily = fontFamily),
 ): SparkTypography = SparkTypography(
+    display1 = display1,
+    display2 = display2,
+    display3 = display3,
+    headline1 = headline1,
+    headline2 = headline2,
+    subhead = subhead,
+    body1 = body1,
+    body1Highlight = body1Highlight,
+    body2 = body2,
+    body2Highlight = body2Highlight,
+    caption = caption,
+    captionHighlight = captionHighlight,
     title1 = title1,
     title2 = title2,
     title3 = title3,
@@ -153,6 +233,7 @@ public fun sparkTypography(
     body = body,
     smallImportant = smallImportant,
     small = small,
+    smallHighlight = smallHighlight,
     extraSmallImportant = extraSmallImportant,
     extraSmall = extraSmall,
     button = button,
@@ -160,17 +241,85 @@ public fun sparkTypography(
 
 @Immutable
 public data class SparkTypography(
+    val display1: TextStyle,
+    val display2: TextStyle,
+    val display3: TextStyle,
+    val headline1: TextStyle,
+    val headline2: TextStyle,
+    val subhead: TextStyle,
+    val body1: TextStyle,
+    val body1Highlight: TextStyle,
+    val body2: TextStyle,
+    val body2Highlight: TextStyle,
+    val caption: TextStyle,
+    val captionHighlight: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("display3"),
+        level = DeprecationLevel.WARNING,
+    )
     val title1: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("headline"),
+        level = DeprecationLevel.WARNING,
+    )
     val title2: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("headline2"),
+        level = DeprecationLevel.WARNING,
+    )
     val title3: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("body1Highlight"),
+        level = DeprecationLevel.WARNING,
+    )
     val largeImportant: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("body1"),
+        level = DeprecationLevel.WARNING,
+    )
     val large: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("body2Highlight"),
+        level = DeprecationLevel.WARNING,
+    )
     val bodyImportant: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("body2"),
+        level = DeprecationLevel.WARNING,
+    )
     val body: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("smallHighlight"),
+        level = DeprecationLevel.WARNING,
+    )
     val smallImportant: TextStyle,
     val small: TextStyle,
+    val smallHighlight: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("captionHighlight"),
+        level = DeprecationLevel.WARNING,
+    )
     val extraSmallImportant: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("caption"),
+        level = DeprecationLevel.WARNING,
+    )
     val extraSmall: TextStyle,
+    @Deprecated(
+        "This property will be removed as it is not part of Spark Token",
+        replaceWith = ReplaceWith("callout"),
+        level = DeprecationLevel.WARNING,
+    )
     val button: TextStyle,
 )
 
@@ -178,15 +327,21 @@ public data class SparkTypography(
 internal val LocalSparkTypography = staticCompositionLocalOf { sparkTypography() }
 
 public fun SparkTypography.asMaterial3Typography(): Typography = Typography(
-    headlineSmall = title1,
-    titleLarge = title2,
-    titleMedium = large,
-    titleSmall = body,
-    bodyLarge = large,
-    bodyMedium = body,
-    labelLarge = button,
+    displayLarge = display1,
+    displayMedium = display2,
+    displaySmall = display3,
+    headlineLarge = headline1,
+    headlineMedium = headline2,
+    headlineSmall = subhead,
+    titleLarge = display1,
+    titleMedium = display2,
+    titleSmall = display3,
+    bodyLarge = body1,
+    bodyMedium = body1,
     bodySmall = small,
-    labelSmall = extraSmall,
+    labelLarge = body1Highlight,
+    labelMedium = body2Highlight,
+    labelSmall = smallHighlight,
 )
 
 @Preview(
