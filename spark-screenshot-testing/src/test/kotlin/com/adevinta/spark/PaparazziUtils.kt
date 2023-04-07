bf43503721d0429041946b4863c52e200d161ee8
Scott Rayapoullé <scott.rayapoulle.ext@adevinta.com>
2023-04-07 17:16:05 +0200

    refactor: Remove the magenta border from screenshots

--- spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/PaparazziUtils.kt
+++ spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/PaparazziUtils.kt
@@ -23,14 +23,12 @@
 package com.adevinta.spark
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.unit.Dp
 import app.cash.paparazzi.Paparazzi
 
 internal fun Paparazzi.sparkSnapshot(
@@ -46,9 +44,7 @@ internal fun Paparazzi.sparkSnapshot(
             Box {
                 // The second box adds a border and background so we can easily see layout bounds in screenshots
                 Box(
-                    Modifier
-                        .border(Dp.Hairline, Color.Magenta)
-                        .background(if (drawBackground) SparkTheme.colors.surface else Color.Transparent),
+                    Modifier.background(if (drawBackground) SparkTheme.colors.surface else Color.Transparent),
                 ) {
                     composable()
                 }
