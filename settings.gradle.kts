cc03fe8dde15537cb4b9266831dd2b1e33b6a3a1
Simon Marquis <simon.marquis@adevinta.com>
2023-04-13 09:21:22 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,7 +34,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://androidx.dev/storage/compose-compiler/repository/") }
     }
 }
 
