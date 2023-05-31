8d21686b9663862a1a7cd0b0a6c48ad3be2584e1
Simon Marquis <simon.marquis@adevinta.com>
2023-05-31 11:53:59 +0200

    Update svg2avd script
    - add `--multipass`
    - no default `--precision` value
    - support windows host

--- scripts/svg2avd/svg2avd.main.kts
+++ scripts/svg2avd/svg2avd.main.kts
@@ -1,33 +1,11 @@
 #!/usr/bin/env kotlin
 
-/*
- * Copyright (c) 2023 Adevinta
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 @file:Repository("https://repo1.maven.org/maven2/")
 @file:Repository("https://maven.google.com")
-@file:DependsOn("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.21")
+
+@file:DependsOn("org.jetbrains.kotlin:kotlin-stdlib:1.9.0-Beta")
 @file:DependsOn("com.android.tools:sdk-common:31.0.2")
 @file:DependsOn("com.github.ajalt.clikt:clikt-jvm:3.5.2")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
 
 import com.android.ide.common.vectordrawable.Svg2Vector
 import com.github.ajalt.clikt.core.CliktCommand
@@ -35,22 +13,20 @@ import com.github.ajalt.clikt.parameters.options.check
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.defaultLazy
 import com.github.ajalt.clikt.parameters.options.flag
-import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.prompt
 import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.types.path
+import java.lang.System.lineSeparator
+import java.nio.file.Files
 import java.nio.file.Path
-import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.absolutePathString
-import kotlin.io.path.deleteExisting
+import kotlin.io.path.createTempDirectory
 import kotlin.io.path.extension
 import kotlin.io.path.isRegularFile
-import kotlin.io.path.name
 import kotlin.io.path.nameWithoutExtension
 import kotlin.io.path.outputStream
 import kotlin.io.path.readText
-import kotlin.io.path.walk
 
 /**
  * SVG2AVD will compress SVG files and convert them to AVD.
@@ -59,62 +35,61 @@ import kotlin.io.path.walk
  */
 class SVG2AVD : CliktCommand(
     name = "svg2avd.main.kts",
-    help = "⚙️ SVG2AVD: Convert SVG files to Android Vector Drawables (AVD).",
+    help = "⚙️ svg2avd: Convert SVG files to Android Vector Drawables (AVD).",
 ) {
 
-    val input: Path by option("-i", "--input", help = "SVG assets input")
+    private val input: Path by option("-i", "--input", help = "SVG assets input directory")
         .path(mustExist = true, canBeFile = false, mustBeReadable = true)
-        .prompt()
+        .prompt("SVG assets input directory")
 
-    val output: Path by option("-o", "--output", help = "AVD assets output")
+    private val output: Path by option("-o", "--output", help = "AVD assets output directory")
         .path(mustExist = true, canBeFile = false, mustBeWritable = true)
         .defaultLazy { input }
 
-    val precision: Int by option("-p", "--precision", help = "svgo compression precision")
-        .help("Set number of digits in the fractional part")
-        .int().default(2)
-        .check("must be a valid integer") { it in 0..10 }
+    private val precision: Int? by option("-p", "--precision", help = "Set number of digits in the fractional part (svgo)")
+        .int().check("must be a positive integer") { it >= 0 }
 
-    val prefix: String by option("-x", "--prefix", help = "XML file prefix")
+    private val prefix: String by option("-x", "--prefix", help = "XML file prefix")
         .default("")
 
-    val copyright: Path? by option("-c", "--copyright", help = "Copyright header for AVD")
+    private val copyright: Path? by option("-c", "--copyright", help = "Copyright header for AVD")
         .path(mustExist = false, canBeDir = false, mustBeReadable = true)
 
-    val quiet by option("-q", "--quiet", help = "Print errors only")
+    private val quiet by option("-q", "--quiet", help = "Print errors only")
         .flag(default = false)
 
-    override fun run() {
-        // 1. Optimize with svgo
-        val svgo = ("svgo " +
-            "--folder ${input.absolutePathString()} " +
-            "--output ${output.absolutePathString()} " +
-            "--precision $precision").exec()
-        if (!quiet) echo(svgo)
+    override fun run() = input
+        .ifNotNull(precision) { compress(precision = it) }
+        .files(extension = "svg")
+        .parallel().forEach { it.convert(output) }
+        .also { if (!quiet) echo(lineSeparator() + "✅ " + output.absolutePathString()) }
 
-        // 2. Convert SVG files to AVD
-        val header = copyright?.takeIf { it.isRegularFile() }?.readText().orEmpty().toByteArray()
-        output.files { it.extension == "svg" }
-            .map { it to output.resolve("$prefix${it.nameWithoutExtension.toSnakeCase()}.xml") }
-            .forEach { (svg, avd) ->
-                if (!quiet) echo("Converting ${svg.name} to ${avd.name}")
-                avd.outputStream().use {
-                    it.write(header)
-                    Svg2Vector.parseSvgToXml(svg, it)
-                    svg.deleteExisting()
-                }
-            }
-
-        // 3. Profit
-        if (!quiet) echo("\n✅ " + output.absolutePathString())
+    private fun Path.compress(precision: Int) = createTempDirectory().also {
+        val svgo = exec(
+            "svgo",
+            "--folder=${absolutePathString()}",
+            "--output=${it.absolutePathString()}",
+            "--precision=$precision",
+            "--multipass",
+        )
+        if (!quiet) echo(svgo + lineSeparator())
     }
-}
 
-@OptIn(ExperimentalPathApi::class)
-fun Path.files() = walk().filter { it: Path -> it.isRegularFile() }
-fun Path.files(predicate: (Path) -> Boolean) = files().filter(predicate)
-fun String.exec() = Runtime.getRuntime().exec(this).text()
-fun Process.text() = apply { waitFor() }.inputStream.bufferedReader().use { it.readText().trim() }
-fun String.toSnakeCase(): String = "(?<=.)[A-Z]".toRegex().replace(this, "_$0").lowercase()
+    private fun Path.convert(output: Path) = output
+        .resolve("$prefix${nameWithoutExtension.toSnakeCase()}.xml")
+        .also { if (!quiet) echo("⚙️ Converting $this to $it") }
+        .outputStream().use {
+            copyright?.takeIf { it.isRegularFile() }?.readText()?.toByteArray()?.let(it::write)
+            Svg2Vector.parseSvgToXml(this, it)
+        }
+
+    private fun Path.files() = Files.walk(this).filter { it: Path -> it.isRegularFile() }
+    private fun Path.files(extension: String) = files().filter { it.extension == extension }
+    private fun exec(vararg cmd: String) = Runtime.getRuntime().exec((arrayOf("cmd", "/c").takeIf { isWindows() } ?: emptyArray()) + cmd).text()
+    private fun Process.text() = apply { waitFor() }.inputStream.bufferedReader().use { it.readText().trim() }
+    private fun String.toSnakeCase(): String = "(?<=.)[A-Z]".toRegex().replace(this, "_$0").lowercase()
+    private fun <I : Any, O : Any?> I.ifNotNull(it: O?, block: I.(O) -> I): I = if (it != null) block(it) else this
+    private fun isWindows() = System.getProperty("os.name").orEmpty().startsWith("windows", ignoreCase = true)
+}
 
 SVG2AVD().main(args)
