1b6bcfd562884f4ee11186d9e07034267ba2bf2c
Simon Marquis <simon.marquis@adevinta.com>
2023-06-05 18:13:50 +0200

    `UpNavigationIcon` uses the default icon size

--- spark/src/main/kotlin/com/adevinta/spark/components/navigation/UpNavigationIcon.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/navigation/UpNavigationIcon.kt
@@ -21,12 +21,10 @@
  */
 package com.adevinta.spark.components.navigation
 
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.adevinta.spark.R
 import com.adevinta.spark.components.icons.Icon
 import com.adevinta.spark.icons.ArrowLeft
@@ -44,7 +42,6 @@ public fun UpNavigationIcon(
         onClick = onClick,
     ) {
         Icon(
-            modifier = Modifier.size(16.dp),
             sparkIcon = SparkIcons.ArrowLeft,
             contentDescription = contentDescription,
         )
