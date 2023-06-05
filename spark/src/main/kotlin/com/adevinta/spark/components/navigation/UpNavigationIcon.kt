eac73a535683eee2b9d28e10b418c2e7daf84e57
Simon Marquis <simon.marquis@adevinta.com>
2023-06-05 16:09:05 +0200

    spotlessApply

--- spark/src/main/kotlin/com/adevinta/spark/components/navigation/UpNavigationIcon.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/navigation/UpNavigationIcon.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.adevinta.spark.R
 import com.adevinta.spark.components.icons.Icon
 import com.adevinta.spark.components.icons.IconSize
