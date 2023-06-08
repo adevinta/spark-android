340ec8fdef9f2b103073ea682d9d29a04405b3fa
Scott Rayapoullé <scott.rayapoulle.ext@adevinta.com>
2023-06-08 15:04:14 +0200

    Restore the font cert resource name

--- spark/src/main/kotlin/com/adevinta/spark/tokens/Font.kt
+++ spark/src/main/kotlin/com/adevinta/spark/tokens/Font.kt
@@ -79,7 +79,7 @@ internal val nunitoFont = GoogleFont("Nunito")
 internal val sparkFontProvider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
     providerPackage = "com.google.android.gms",
-    certificates = R.array.spark_com_google_android_gms_fonts_certs,
+    certificates = R.array.com_google_android_gms_fonts_certs,
 )
 
 internal val nunitoFontFamily = FontFamily(
