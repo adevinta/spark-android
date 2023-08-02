b4fc60d2ab3fecaafedf3f2a6ac2968fc3911013
Marina Romanova <marina.romanova@adevinta.com>
2023-08-02 17:44:01 +0200

    Update buttons

--- spark/src/main/kotlin/com/adevinta/spark/components/buttons/Button.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/buttons/Button.kt
@@ -45,6 +45,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
@@ -231,9 +232,6 @@ public object SparkButtonDefaults {
         horizontal = 16.dp,
         vertical = size.contentVerticalPadding,
     )
-
-    internal val Color.disabled: Color
-        @Composable get() = this.dim3
 
     /**
      * The default border of [ButtonOutlined]
