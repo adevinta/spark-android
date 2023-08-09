46b2aa674f59a4630db6bf84415f1784cb08d5ae
Theo Bertacchini <theo.bertacchini@adevinta.com>
2023-08-09 15:43:51 +0200

    feat: save RadioButtonConfigurator state in datastore
    Change-Id: Ia2761002d36de366dc6f2bb2d3358b2424705087

--- catalog/src/main/kotlin/com/adevinta/spark/catalog/configurator/samples/toggles/RadioButtonConfigurator.kt
+++ catalog/src/main/kotlin/com/adevinta/spark/catalog/configurator/samples/toggles/RadioButtonConfigurator.kt
@@ -29,18 +29,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import com.adevinta.spark.SparkTheme
 import com.adevinta.spark.catalog.R
+import com.adevinta.spark.catalog.datastore.radiobuttonconfigurator.RadioButtonConfiguratorProperties
+import com.adevinta.spark.catalog.datastore.radiobuttonconfigurator.RadioButtonConfiguratorPropertiesHandler
 import com.adevinta.spark.catalog.model.Configurator
 import com.adevinta.spark.catalog.themes.SegmentedButton
 import com.adevinta.spark.catalog.util.SampleSourceUrl
@@ -53,6 +59,7 @@ import com.adevinta.spark.components.toggles.RadioButton
 import com.adevinta.spark.components.toggles.RadioButtonLabelled
 import com.adevinta.spark.components.toggles.SwitchLabelled
 import com.adevinta.spark.components.toggles.ToggleIntent
+import kotlinx.coroutines.launch
 
 public val RadioButtonConfigurator: Configurator = Configurator(
     name = "RadioButton",
@@ -68,21 +75,36 @@ public val RadioButtonConfigurator: Configurator = Configurator(
 @Composable
 private fun RadioButtonSample() {
     val scrollState = rememberScrollState()
+    val context = LocalContext.current
     val focusManager = LocalFocusManager.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val propertiesHandler = remember { RadioButtonConfiguratorPropertiesHandler(context) }
+    val properties by propertiesHandler
+        .properties
+        .collectAsState(initial = RadioButtonConfiguratorProperties.DEFAULT)
+
+    fun updateProperties(block: (RadioButtonConfiguratorProperties) -> RadioButtonConfiguratorProperties) {
+        lifecycleOwner.lifecycleScope.launch {
+            propertiesHandler.updateProperties(block(properties))
+        }
+    }
+
     Column(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier.verticalScroll(scrollState),
     ) {
-        var isEnabled by remember { mutableStateOf(true) }
-        var contentSide by remember { mutableStateOf(ContentSide.End) }
         var label: String? by remember { mutableStateOf(null) }
-        var selected by remember { mutableStateOf(false) }
-        var intent by remember { mutableStateOf(ToggleIntent.Main) }
-        val onClick = { selected = !selected }
+        val isEnabled: Boolean = remember(key1 = properties.isEnabled, calculation = properties::isEnabled)
+        val isSelected: Boolean = remember(key1 = properties.isSelected, calculation = properties::isSelected)
+        val contentSide: ContentSide = remember(key1 = properties.contentSide, calculation = properties::contentSide)
+        val intent: ToggleIntent = remember(key1 = properties.intent, calculation = properties::intent)
+        val onClick = {
+            updateProperties { it.copy(isSelected = !it.isSelected) }
+        }
         ConfigedRadioButton(
             label = label,
             onClick = onClick,
-            selected = selected,
+            selected = isSelected,
             isEnabled = isEnabled,
             intent = intent,
             contentSide = contentSide,
@@ -90,7 +112,7 @@ private fun RadioButtonSample() {
         SwitchLabelled(
             checked = isEnabled,
             onCheckedChange = {
-                isEnabled = it
+                updateProperties { properties -> properties.copy(isEnabled = it) }
                 focusManager.clearFocus()
             },
         ) {
@@ -115,7 +137,7 @@ private fun RadioButtonSample() {
                     DropdownMenuItem(
                         text = { Text(it.name) },
                         onClick = {
-                            intent = it
+                            updateProperties { properties -> properties.copy(intent = it) }
                             expanded = false
                         },
                     )
@@ -134,7 +156,7 @@ private fun RadioButtonSample() {
                 options = contentSidesLabel,
                 selectedOption = contentSide.name,
                 onOptionSelect = {
-                    contentSide = ContentSide.valueOf(it)
+                    updateProperties { properties -> properties.copy(contentSide = ContentSide.valueOf(it)) }
                     focusManager.clearFocus()
                 },
                 modifier = Modifier
