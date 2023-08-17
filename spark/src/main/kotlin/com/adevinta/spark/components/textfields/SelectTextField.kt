e915ec30340168a29084a42bb71d4ee026e9eb6a
Marina Romanova <36896406+MarinaRomanova@users.noreply.github.com>
2023-08-17 10:13:32 +0200

    feat: Add IconButtons with all intents, shapes and sizes (#612)
    (cherry picked from commit c0b28f6fc8bf9d0080be5dc2caf142be2df097da)

--- spark/src/main/kotlin/com/adevinta/spark/components/textfields/SelectTextField.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/textfields/SelectTextField.kt
@@ -19,6 +19,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+@file:Suppress("DEPRECATION")
+
 package com.adevinta.spark.components.textfields
 
 import androidx.compose.foundation.interaction.FocusInteraction
