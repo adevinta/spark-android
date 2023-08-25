4dabe67335159b8e07ae9d9b618c09b78c81dacb
Ahmed Elshahawy <ahmed.elshahawy@adevinta.com>
2023-08-25 11:25:55 +0200

    Update package

--- spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/iconbutton/toggle/IconToggleButtonScreenshot.kt
+++ spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/iconbutton/toggle/IconToggleButtonScreenshot.kt
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.adevinta.spark.icontogglebutton
+package com.adevinta.spark.iconbutton.toggle
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
