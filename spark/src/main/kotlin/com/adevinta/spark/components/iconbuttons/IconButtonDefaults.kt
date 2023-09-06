0eb6aaf13b624cbe57996ec6fc5ba5b18c015297
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2023-09-06 14:34:30 +0200

    chore(iconbutton): Add screenshot tests

--- spark/src/main/kotlin/com/adevinta/spark/components/iconbuttons/IconButtonDefaults.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/iconbuttons/IconButtonDefaults.kt
@@ -30,7 +30,7 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
 import com.adevinta.spark.SparkTheme
 import com.adevinta.spark.components.IntentColor
-import com.adevinta.spark.tokens.disabled
+import com.adevinta.spark.tokens.dim3
 
 internal object IconButtonDefaults {
 
@@ -75,8 +75,8 @@ internal object IconButtonDefaults {
         intent: IntentColor,
         containerColor: Color = intent.color,
         contentColor: Color = intent.onColor,
-        disabledContainerColor: Color = containerColor.disabled,
-        disabledContentColor: Color = contentColor.disabled,
+        disabledContainerColor: Color = containerColor.dim3,
+        disabledContentColor: Color = contentColor.dim3,
     ): IconButtonColors = IconButtonColors(
         containerColor = containerColor,
         contentColor = contentColor,
@@ -90,7 +90,7 @@ internal object IconButtonDefaults {
         containerColor: Color = Color.Transparent,
         contentColor: Color = intent.color,
         disabledContainerColor: Color = Color.Transparent,
-        disabledContentColor: Color = contentColor.disabled,
+        disabledContentColor: Color = contentColor.dim3,
     ): IconButtonColors = IconButtonColors(
         containerColor = containerColor,
         contentColor = contentColor,
@@ -103,8 +103,8 @@ internal object IconButtonDefaults {
         intent: IntentColor,
         containerColor: Color = intent.containerColor,
         contentColor: Color = intent.onContainerColor,
-        disabledContainerColor: Color = containerColor.disabled,
-        disabledContentColor: Color = contentColor.disabled,
+        disabledContainerColor: Color = containerColor.dim3,
+        disabledContentColor: Color = contentColor.dim3,
     ): IconButtonColors = IconButtonColors(
         containerColor = containerColor,
         contentColor = contentColor,
@@ -118,7 +118,7 @@ internal object IconButtonDefaults {
         containerColor: Color = Color.Transparent,
         contentColor: Color = intent.color,
         disabledContainerColor: Color = Color.Transparent,
-        disabledContentColor: Color = contentColor.disabled,
+        disabledContentColor: Color = contentColor.dim3,
     ): IconButtonColors = IconButtonColors(
         containerColor = containerColor,
         contentColor = contentColor,
@@ -131,13 +131,13 @@ internal object IconButtonDefaults {
         intent: IntentColor,
         containerColor: Color = SparkTheme.colors.surface,
         contentColor: Color = intent.color,
-        disabledContainerColor: Color = containerColor.disabled,
+        disabledContainerColor: Color = containerColor.dim3,
     ): IconButtonColors = with(if (containerColor != contentColor) contentColor else SparkTheme.colors.onSurface) {
         IconButtonColors(
             containerColor = containerColor,
             contentColor = this,
             disabledContainerColor = disabledContainerColor,
-            disabledContentColor = this.disabled,
+            disabledContentColor = this.dim3,
         )
     }
 }
