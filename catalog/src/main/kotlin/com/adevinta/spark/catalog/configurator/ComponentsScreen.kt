15c6579b1e71fcc91208ca36dfdd6d55ae6452ba
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2023-09-19 16:59:48 +0200

    feat(catalog): Add illustrations for components item (#715)

--- catalog/src/main/kotlin/com/adevinta/spark/catalog/configurator/ComponentsScreen.kt
+++ catalog/src/main/kotlin/com/adevinta/spark/catalog/configurator/ComponentsScreen.kt
@@ -45,7 +45,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.adevinta.spark.SparkTheme
 import com.adevinta.spark.catalog.R
-import com.adevinta.spark.catalog.configurator.component.ConfiguratorComponentComponentItem
+import com.adevinta.spark.catalog.examples.component.ComponentItem
 import com.adevinta.spark.catalog.model.Component
 import com.adevinta.spark.components.text.Text
 import com.adevinta.spark.tokens.Layout
@@ -126,11 +126,15 @@ internal fun ComponentsListScreen(
             span = { GridItemSpan(1) },
             contentType = { ComponentsItemType.Component },
             itemContent = { component ->
-                ConfiguratorComponentComponentItem(component = component) {
-                    val componentId = component.id
-                    val route = "$ConfiguratorRoute/$componentId"
-                    navController.navigate(route)
-                }
+                ComponentItem(
+                    component = component,
+                    showExampleCount = false,
+                    onClick = {
+                        val componentId = component.id
+                        val route = "$ConfiguratorRoute/$componentId"
+                        navController.navigate(route)
+                    },
+                )
             },
         )
     }
