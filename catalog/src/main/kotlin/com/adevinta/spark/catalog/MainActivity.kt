ba9fc7baffb8bbee31fec3a0a2ba09ba620acba3
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2023-11-07 10:38:23 +0100

    Apply spotless

--- catalog/src/main/kotlin/com/adevinta/spark/catalog/MainActivity.kt
+++ catalog/src/main/kotlin/com/adevinta/spark/catalog/MainActivity.kt
@@ -22,7 +22,6 @@
 package com.adevinta.spark.catalog
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
