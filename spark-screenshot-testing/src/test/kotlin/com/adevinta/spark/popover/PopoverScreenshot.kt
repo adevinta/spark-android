e995fa3e4828d55e307ebea37cae7e2de5096d95
Ahmed Elshahawy <ahmed.elshahawy@adevinta.com>
2023-11-07 13:55:15 +0100

    Apply Spotless

--- spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/popover/PopoverScreenshot.kt
+++ spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/popover/PopoverScreenshot.kt
@@ -127,7 +127,6 @@ internal class PopoverScreenshot {
                                     onClick = { scope.launch { state.show() } },
                                     interactionSource = interactionSource,
                                 )
-
                             }
                         }
                     }
