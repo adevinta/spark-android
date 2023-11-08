358f6c0249a0bd506079c520ec9461adc6af319b
Simon Marquis <simon.marquis@adevinta.com>
2023-11-08 10:17:36 +0100

    Update settings.gradle.kts
    feat(maven): add repository for pre-release artifacts of compose-compiler

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,6 +33,15 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
+        // Pre-release artifacts of compose-compiler
+        // https://androidx.dev/storage/compose-compiler/repository
+        maven("https://androidx.dev/storage/compose-compiler/repository/") {
+            name = "compose-compiler-dev"
+            content {
+                includeGroup("androidx.compose.compiler")
+            }
+        }
     }
 }
 
