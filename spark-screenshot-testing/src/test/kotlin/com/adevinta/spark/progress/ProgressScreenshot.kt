dfe6587a271075f6cfd69d60ebea121f12cd98cc
Ahmed Elshahawy <ahmed.elshahawy@adevinta.com>
2023-11-23 10:14:59 +0100

    Add screenshots

---
+++ spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/progress/ProgressScreenshot.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023 Adevinta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.adevinta.spark.progress
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.Paparazzi
+import com.adevinta.spark.MaxPercentDifference
+import com.adevinta.spark.PaparazziTheme
+import com.adevinta.spark.components.progressbar.Progressbar
+import com.adevinta.spark.components.progressbar.ProgressbarIndeterminate
+import com.adevinta.spark.components.progressbar.ProgressbarIntent
+import com.adevinta.spark.components.text.Text
+import com.adevinta.spark.patchedEnvironment
+import com.adevinta.spark.sparkSnapshot
+import com.android.ide.common.rendering.api.SessionParams
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.random.Random
+
+@RunWith(TestParameterInjector::class)
+internal class ProgressScreenshot {
+
+    private val isRoundedList: List<Boolean> = listOf(true, false)
+
+    private val isIntermediateList: List<Boolean> = listOf(true, false)
+
+    private val intents = ProgressbarIntent.entries.toTypedArray()
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        maxPercentDifference = MaxPercentDifference,
+        theme = PaparazziTheme,
+        renderingMode = SessionParams.RenderingMode.SHRINK,
+        showSystemUi = true,
+        environment = patchedEnvironment(),
+        deviceConfig = app.cash.paparazzi.DeviceConfig.PIXEL_C.copy(
+            softButtons = false,
+            locale = "fr-rFR",
+        ),
+    )
+
+    @Test
+    fun test() {
+        isRoundedList.forEach { isRounded ->
+            isIntermediateList.forEach { isIntermediate ->
+                paparazzi.sparkSnapshot(
+                    name = "$isRounded" +
+                        "_$isIntermediate",
+                ) {
+                    Row {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            Text(
+                                text = "isRounded: $isRounded, isIntermediate: $isIntermediate",
+                            )
+                            intents.forEach { intent ->
+                                val progress: Double = Random.nextDouble(0.0, 1.0)
+
+                                Column {
+                                    Progressbar(
+                                        intent = intent,
+                                        modifier = Modifier.fillMaxWidth(),
+                                        progress = progress.toFloat(),
+                                        isRounded = Random.nextBoolean(),
+                                    )
+                                    ProgressbarIndeterminate(
+                                        intent = intent,
+                                        modifier = Modifier.fillMaxWidth(),
+                                        isRounded = Random.nextBoolean(),
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
