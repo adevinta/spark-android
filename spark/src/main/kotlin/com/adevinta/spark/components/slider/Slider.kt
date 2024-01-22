e477fdc77386836935ac6bddc768d638d1ef3005
Ahmed Elshahawy <ahmed.elshahawy@adevinta.com>
2024-01-22 17:14:43 +0100

    Init Slider component

--- spark/src/main/kotlin/com/adevinta/spark/components/slider/Slider.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/slider/Slider.kt
@@ -21,42 +21,69 @@
  */
 package com.adevinta.spark.components.slider
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.DragInteraction
+import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SliderColors
 import androidx.compose.material3.SliderPositions
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.lerp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
 import com.adevinta.spark.ExperimentalSparkApi
 import com.adevinta.spark.InternalSparkApi
 import com.adevinta.spark.PreviewTheme
+import com.adevinta.spark.SparkTheme
+import com.adevinta.spark.tokens.dim3
+import com.adevinta.spark.tokens.dim4
 import com.adevinta.spark.tools.preview.ThemeProvider
 import com.adevinta.spark.tools.preview.ThemeVariant
 import androidx.compose.material3.Slider as MaterialSlider
-import androidx.compose.material3.SliderDefaults as MaterialSliderDefaults
 
 @OptIn(ExperimentalMaterial3Api::class)
 @InternalSparkApi
 @Composable
 internal fun SparkSlider(
     value: Float,
+    intent: SliderIntent,
     onValueChange: (Float) -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
-    /*@IntRange(from = 0)*/
     steps: Int = 0,
     onValueChangeFinished: (() -> Unit)? = null,
-    colors: SliderColors = SliderDefaults.colors(),
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    thumb: @Composable (SliderPositions) -> Unit = remember(interactionSource, colors, enabled) {
+    handle: @Composable (SliderPositions) -> Unit = remember(interactionSource, enabled) {
         {
-            MaterialSliderDefaults.Thumb(
+            Handle(
                 interactionSource = interactionSource,
-                colors = colors,
+                intent = intent,
                 enabled = enabled,
             )
         }
@@ -70,10 +97,175 @@ internal fun SparkSlider(
         valueRange = valueRange,
         steps = steps,
         onValueChangeFinished = onValueChangeFinished,
-        colors = colors,
         interactionSource = interactionSource,
-        thumb = thumb,
+        thumb = handle,
+        track = { sliderPositions ->
+            Track(
+                intent = intent,
+                enabled = enabled,
+                sliderPositions = sliderPositions,
+            )
+        },
     )
+}
+
+private val HandleDefaultOutline = 1.dp
+private val DefaultHandleSizeInnerWidth = 24.dp
+private val DefaultHandleSizeWidth = 32.dp
+private val DefaultHandleSize = DpSize(DefaultHandleSizeWidth, DefaultHandleSizeWidth)
+
+@Composable
+internal fun Handle(
+    intent: SliderIntent = SliderIntent.Accent,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    enabled: Boolean = true,
+    handleSize: DpSize = DefaultHandleSize,
+) {
+    val interactions = remember { mutableStateListOf<Interaction>() }
+    LaunchedEffect(interactionSource) {
+        interactionSource.interactions.collect { interaction ->
+            when (interaction) {
+                is PressInteraction.Press -> interactions.add(interaction)
+                is PressInteraction.Release -> interactions.remove(interaction.press)
+                is PressInteraction.Cancel -> interactions.remove(interaction.press)
+                is DragInteraction.Start -> interactions.add(interaction)
+                is DragInteraction.Stop -> interactions.remove(interaction.start)
+                is DragInteraction.Cancel -> interactions.remove(interaction.start)
+            }
+        }
+    }
+
+    val handleColor = rememberUpdatedState(
+        if (enabled) {
+            intent.colors().color
+        } else intent.colors().color.dim3,
+    ).value
+
+    val handleColorBg = rememberUpdatedState(
+        if (interactions.isNotEmpty()) {
+            SparkTheme.colors.basicContainer
+        } else Color.Unspecified,
+    ).value
+
+    val handleColorBorder = rememberUpdatedState(
+        if (interactions.isNotEmpty()) {
+            intent.colors().color
+        } else Color.Unspecified,
+    ).value
+
+    Box(contentAlignment = Alignment.Center) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(DefaultHandleSizeWidth)
+                .background(handleColorBg, CircleShape)
+                .border(
+                    width = HandleDefaultOutline,
+                    color = handleColorBorder,
+                    shape = CircleShape,
+                ),
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(DefaultHandleSizeInnerWidth)
+                    .background(SparkTheme.colors.basicContainer, CircleShape),
+            )
+            Box(
+                modifier = Modifier
+                    .size(DefaultHandleSizeInnerWidth)
+                    .background(handleColor, CircleShape)
+                    .hoverable(interactionSource = interactionSource),
+            )
+            Box(
+                Modifier
+                    .size(handleSize)
+                    .hoverable(interactionSource = interactionSource),
+
+                )
+        }
+    }
+}
+
+internal val TrackHeight = 4.dp
+private val TickSize = 2.dp
+
+
+/**
+ * The Default track for [Slider] and [RangeSlider]
+ *
+ * @param sliderPositions [SliderPositions] which is used to obtain the current active track
+ * and the tick positions if the slider is discrete.
+ * @param modifier the [Modifier] to be applied to the track.
+ * @param enabled controls the enabled state of this slider. When `false`, this component will
+ * not respond to user input, and it will appear visually disabled and disabled to
+ * accessibility services.
+ */
+@Composable
+internal fun Track(
+    sliderPositions: SliderPositions,
+    modifier: Modifier = Modifier,
+    intent: SliderIntent = SliderIntent.Accent,
+    enabled: Boolean = true,
+) {
+    val indicatorColor = rememberUpdatedState(
+        if (enabled) {
+            intent.colors().color
+        } else intent.colors().color.dim3,
+    ).value
+
+    val trackColor = SparkTheme.colors.onBackground.dim4
+
+    val tickActiveColor = intent.colors().onColor
+    val tickInactiveColor = intent.colors().color
+
+    Canvas(
+        modifier
+            .fillMaxWidth()
+            .height(TrackHeight),
+    ) {
+        val isRtl = layoutDirection == LayoutDirection.Rtl
+        val sliderLeft = Offset(0f, center.y)
+        val sliderRight = Offset(size.width, center.y)
+        val sliderStart = if (isRtl) sliderRight else sliderLeft
+        val sliderEnd = if (isRtl) sliderLeft else sliderRight
+        val tickSize = TickSize.toPx()
+        val trackStrokeWidth = TrackHeight.toPx()
+        drawLine(
+            trackColor,
+            sliderStart,
+            sliderEnd,
+            trackStrokeWidth,
+            StrokeCap.Round,
+        )
+        val sliderValueEnd = Offset(
+            sliderStart.x + (sliderEnd.x - sliderStart.x) * sliderPositions.activeRange.endInclusive,
+            center.y,
+        )
+
+        val sliderValueStart = Offset(
+            sliderStart.x + (sliderEnd.x - sliderStart.x) * sliderPositions.activeRange.start,
+            center.y,
+        )
+
+        drawLine(
+            indicatorColor,
+            sliderValueStart,
+            sliderValueEnd,
+            trackStrokeWidth,
+            StrokeCap.Round,
+        )
+        sliderPositions.tickFractions.groupBy {
+            it > sliderPositions.activeRange.endInclusive || it < sliderPositions.activeRange.start
+        }.forEach { (outsideFraction, list) ->
+            drawPoints(
+                list.map { Offset(lerp(sliderStart, sliderEnd, it).x, center.y) },
+                PointMode.Points,
+                (if (outsideFraction) tickInactiveColor else tickActiveColor),
+                tickSize,
+                StrokeCap.Round,
+            )
+        }
+    }
 }
 
 /**
@@ -105,8 +297,8 @@ public fun Slider(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
-    /*@IntRange(from = 0)*/
     steps: Int = 0,
+    intent: SliderIntent = SliderIntent.Basic,
     onValueChangeFinished: (() -> Unit)? = null,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
 ) {
@@ -114,6 +306,7 @@ public fun Slider(
         value = value,
         onValueChange = onValueChange,
         modifier = modifier,
+        intent = intent,
         enabled = enabled,
         valueRange = valueRange,
         steps = steps,
@@ -130,16 +323,21 @@ public fun Slider(
 internal fun SliderPreview(
     @PreviewParameter(ThemeProvider::class) theme: ThemeVariant,
 ) {
+    var progress by remember { mutableFloatStateOf(0.75f) }
+
     PreviewTheme(theme) {
-        Slider(
-            value = 1f,
-            onValueChange = {},
-            enabled = true,
-            steps = 3,
-        )
 
         Slider(
+            value = progress,
+            intent = SliderIntent.Success,
+            onValueChange = { progress = it },
+            enabled = true,
+            valueRange = 0f..1f,
+            steps = 3,
+        )
+        Slider(
             value = 0.5f,
+            intent = SliderIntent.Success,
             onValueChange = {},
             enabled = false,
         )
