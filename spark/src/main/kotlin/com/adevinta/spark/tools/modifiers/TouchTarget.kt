2699352fcbff34bf8ca95f99bb119edb1a973660
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2024-02-07 17:01:58 +0100

    Revert change on the minimumTargetSize modifier as it changes too many layouts for now

--- spark/src/main/kotlin/com/adevinta/spark/tools/modifiers/TouchTarget.kt
+++ spark/src/main/kotlin/com/adevinta/spark/tools/modifiers/TouchTarget.kt
@@ -21,20 +21,77 @@
  */
 package com.adevinta.spark.tools.modifiers
 
-import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.node.LayoutModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.platform.InspectorInfo
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 /**
- * Reserves at least 48.dp in size to disambiguate touch interactions if the element would measure
+ * Reserves at least 44.dp in size to disambiguate touch interactions if the element would measure
  * smaller.
  *
  * https://m3.material.io/foundations/accessible-design/accessibility-basics#28032e45-c598-450c-b355-f9fe737b1cd8
  *
- * This uses the Material recommended minimum size of 48.dp x 48.dp, which may not the same as the
- * system enforced minimum size. The minimum clickable / touch target size (48.dp by default) is
+ * This uses the Spark recommended minimum size of 44.dp x 44.dp, which may not the same as the
+ * system enforced minimum size. The minimum clickable / touch target size (44.dp by default) is
  * controlled by the system via ViewConfiguration` and automatically expanded at the touch input layer.
  *
  * This modifier is not needed for touch target expansion to happen. It only affects layout, to make
  * sure there is adequate space for touch target expansion.
  */
-public fun Modifier.minimumTouchTargetSize(): Modifier = this then minimumInteractiveComponentSize()
+public fun Modifier.minimumTouchTargetSize(): Modifier = this then MinimumTouchTargetModifier()
+
+private class MinimumTouchTargetModifier : ModifierNodeElement<MinimumTouchTargetModifierNode>() {
+
+    override fun create() = MinimumTouchTargetModifierNode()
+    override fun equals(other: Any?) = (other === this)
+
+    override fun hashCode(): Int = System.identityHashCode(this)
+
+    override fun update(node: MinimumTouchTargetModifierNode) = Unit
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "minimumTouchTargetSize"
+        properties["README"] = "Adds outer padding to measure at least 44.dp (default) in " +
+            "size to disambiguate touch interactions if the element would measure smaller"
+    }
+}
+
+private class MinimumTouchTargetModifierNode : Modifier.Node(), LayoutModifierNode {
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints,
+    ): MeasureResult {
+        val size = minimumTouchTargetSize
+        val placeable = measurable.measure(constraints)
+        val enforcement = isAttached
+
+        // Be at least as big as the minimum dimension in both dimensions
+        val width = if (enforcement) {
+            maxOf(placeable.width, size.width.roundToPx())
+        } else {
+            placeable.width
+        }
+        val height = if (enforcement) {
+            maxOf(placeable.height, size.height.roundToPx())
+        } else {
+            placeable.height
+        }
+
+        return layout(width, height) {
+            val centerX = ((width - placeable.width) / 2f).roundToInt()
+            val centerY = ((height - placeable.height) / 2f).roundToInt()
+            placeable.place(centerX, centerY)
+        }
+    }
+}
+
+private val minimumTouchTargetSize = DpSize(44.dp, 44.dp)
