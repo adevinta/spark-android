c2af4f77eb9fa5ff633c1c8c8665ef94106c3746
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2024-03-05 11:53:57 +0100

    Apply spotless

--- spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/textfields/MultilineTextFieldScreenshot.kt
+++ spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/textfields/MultilineTextFieldScreenshot.kt
@@ -331,7 +331,7 @@ internal class MultilineTextFieldScreenshot {
     companion object {
         private const val stubShortBody = "Lorem ipsum"
         private const val stubBody = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lacus dolor, " +
-                "pulvinar eu nulla sit amet, iaculis interdum."
+            "pulvinar eu nulla sit amet, iaculis interdum."
     }
 
     internal abstract class DefaultMutableInteractionSource : MutableInteractionSource {
