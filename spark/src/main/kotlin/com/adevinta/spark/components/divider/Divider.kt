5841441ec81b5018cb1f98e6f6c5a7b0dee9e9da
Ahmed Elshahawy <ahmed.elshahawy@adevinta.com>
2024-03-14 17:20:34 +0100

    Fix divider component after m3 upgrade

--- spark/src/main/kotlin/com/adevinta/spark/components/divider/Divider.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/divider/Divider.kt
@@ -21,80 +21,90 @@
  */
 package com.adevinta.spark.components.divider
 
-import androidx.compose.material3.DividerDefaults
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.Dp
-import com.adevinta.spark.ExperimentalSparkApi
 import com.adevinta.spark.PreviewTheme
 import com.adevinta.spark.SparkTheme
 import com.adevinta.spark.tokens.dim3
 import com.adevinta.spark.tools.preview.ThemeProvider
 import com.adevinta.spark.tools.preview.ThemeVariant
-import androidx.compose.material3.HorizontalDivider as MaterialDivider
-import androidx.compose.material3.VerticalDivider as MaterialVerticalDivider
 
-@ExperimentalSparkApi
 @Composable
-internal fun SparkDivider(
+internal fun SparkHorizontalDivider(
     modifier: Modifier = Modifier,
-    color: Color = SparkTheme.colors.outline,
     thickness: Dp = DividerDefaults.Thickness,
+    color: Color = SparkTheme.colors.outline,
 ) {
-    MaterialDivider(
-        modifier,
-        thickness,
-        color,
-    )
+    Canvas(
+        modifier
+            .fillMaxWidth()
+            .height(thickness),
+    ) {
+        drawLine(
+            color = color,
+            strokeWidth = thickness.toPx(),
+            start = Offset(0f, thickness.toPx() / 2),
+            end = Offset(size.width, thickness.toPx() / 2),
+        )
+    }
+}
+
+@Composable
+internal fun SparkVerticalDivider(
+    modifier: Modifier = Modifier,
+    thickness: Dp = DividerDefaults.Thickness,
+    color: Color = SparkTheme.colors.outline,
+) {
+    Canvas(
+        modifier
+            .fillMaxHeight()
+            .width(thickness),
+    ) {
+        drawLine(
+            color = color,
+            strokeWidth = thickness.toPx(),
+            start = Offset(thickness.toPx() / 2, 0f),
+            end = Offset(thickness.toPx() / 2, size.height),
+        )
+    }
 }
 
 /**
- * Spark divider.
+ * A horizontal divider that can be used to separate content.
  *
- * A divider is a thin line that groups content in lists and layouts.
- *
- * ![Divider image](https://developer.android.com/images/reference/androidx/compose/material3/divider.png)
- *
- * @param modifier the [Modifier] to be applied to this divider line.
- * @param color the color of the divider, we recommend to use either outline or onSurface dim 3.
+ * @param modifier The modifier to be applied to the divider.
+ * @param color The color of the divider.
  */
-@ExperimentalSparkApi
 @Composable
 public fun Divider(
     modifier: Modifier = Modifier,
     color: Color = SparkTheme.colors.onSurface.dim3,
 ) {
-    SparkDivider(
-        modifier = modifier,
-        thickness = DividerDefaults.Thickness,
-        color = color,
-    )
+    SparkHorizontalDivider(modifier = modifier, color = color)
 }
 
 /**
- * Spark divider.
+ * A vertical divider that can be used to separate content.
  *
- * A divider is a thin line that groups content in lists and layouts.
- *
- * ![Divider image](https://developer.android.com/images/reference/androidx/compose/material3/divider.png)
- *
- * @param modifier the [Modifier] to be applied to this divider line.
- * @param color the color of the divider, we recommend to use either outline or onSurface dim 3.
+ * @param modifier The modifier to be applied to the divider.
+ * @param color The color of the divider.
  */
-@ExperimentalSparkApi
 @Composable
 public fun VerticalDivider(
     modifier: Modifier = Modifier,
     color: Color = SparkTheme.colors.onSurface.dim3,
 ) {
-    MaterialVerticalDivider(
-        modifier = modifier,
-        thickness = DividerDefaults.Thickness,
-        color = color,
-    )
+    SparkVerticalDivider(modifier = modifier, color = color)
 }
 
 @Preview(
@@ -107,6 +117,6 @@ internal fun DividerPreview(
 ) {
     PreviewTheme(theme) {
         Divider()
-        Divider()
+        VerticalDivider()
     }
 }
