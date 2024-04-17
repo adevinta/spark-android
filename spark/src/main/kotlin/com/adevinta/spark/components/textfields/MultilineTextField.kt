a43959d44505aa7bde074fd1f4225f8a2b956169
Scott Rayapoullé <scott.rayapoulle@adevinta.com>
2024-04-17 11:47:22 +0200

    Apply spotless

--- spark/src/main/kotlin/com/adevinta/spark/components/textfields/MultilineTextField.kt
+++ spark/src/main/kotlin/com/adevinta/spark/components/textfields/MultilineTextField.kt
@@ -43,7 +43,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.adevinta.spark.PreviewTheme
 import com.adevinta.spark.R
 import com.adevinta.spark.components.icons.Icon
@@ -54,8 +53,6 @@ import com.adevinta.spark.icons.DeleteOutline
 import com.adevinta.spark.icons.LikeFill
 import com.adevinta.spark.icons.SparkIcons
 import com.adevinta.spark.tokens.SparkTypography
-import com.adevinta.spark.tools.preview.ThemeProvider
-import com.adevinta.spark.tools.preview.ThemeVariant
 import kotlinx.coroutines.flow.flowOf
 
 /**
@@ -305,7 +302,7 @@ public fun MultilineTextField(
 )
 @Composable
 private fun MultilineTextFieldIntentPreview() {
-    PreviewTheme() {
+    PreviewTheme {
         PreviewTextFields(
             state = null,
             stateMessage = "Helper text",
