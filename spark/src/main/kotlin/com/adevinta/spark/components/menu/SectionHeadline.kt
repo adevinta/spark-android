ca2a0ce49f24e7598d8f6beeedcc96c55ecf37d1
Ahmed Elshahawy <ahmed.elshahawy@adevinta.com>
2024-04-25 03:57:51 +0200

    Add Dropdown - Section Headline

---
+++ spark/src/main/kotlin/com/adevinta/spark/components/menu/SectionHeadline.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2024 Adevinta
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.adevinta.spark.components.menu
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.adevinta.spark.SparkTheme
+import com.adevinta.spark.components.text.Text
+import com.adevinta.spark.tokens.dim1
+import com.adevinta.spark.tokens.highlight
+
+@Composable
+public fun SectionHeadline(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    ProvideTextStyle(value = SparkTheme.typography.body2) {
+        CompositionLocalProvider(LocalContentColor provides SparkTheme.colors.onSurface.dim1) {
+            Box(modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+                content()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SectionPrev() {
+    SectionHeadline {
+        Text(
+            "Section headline",
+            style = SparkTheme.typography.body1.highlight,
+            color = SparkTheme.colors.basic,
+        )
+    }
+}
