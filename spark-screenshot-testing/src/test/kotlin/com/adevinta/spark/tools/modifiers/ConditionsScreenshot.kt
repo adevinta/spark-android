cbe15fc56ea6482461f8b5f22fa64d0475ee283e
Eliott Lujan <eliott.lujan@adevinta.com>
2025-03-21 16:43:23 +0100

    Move TestComposables

--- spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/tools/modifiers/ConditionsScreenshot.kt
+++ spark-screenshot-testing/src/test/kotlin/com/adevinta/spark/tools/modifiers/ConditionsScreenshot.kt
@@ -22,10 +22,24 @@
 
 package com.adevinta.spark.tools.modifiers
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import app.cash.paparazzi.DeviceConfig
 import com.adevinta.spark.paparazziRule
 import com.adevinta.spark.sparkSnapshot
@@ -95,6 +109,92 @@ class ConditionsScreenshot {
                     modifier = Modifier.rotate(90f),
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun IfTrueTestComposable(
+    value: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    ConditionTestComposable(
+        conditionName = "ifTrue",
+        conditionValue = value,
+        conditionModifier = modifier
+            .size(100.dp)
+            .background(Color.Red)
+            .ifTrue(value) { background(Color.Green) },
+    )
+}
+
+@Composable
+fun IfFalseTestComposable(
+    value: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    ConditionTestComposable(
+        conditionName = "ifFalse",
+        conditionValue = value,
+        conditionModifier = modifier
+            .size(100.dp)
+            .background(Color.Red)
+            .ifFalse(value) { background(Color.Green) },
+    )
+}
+
+@Composable
+fun IfNullTestComposable(
+    value: Any?,
+    modifier: Modifier = Modifier,
+) {
+    ConditionTestComposable(
+        conditionName = "ifNull",
+        conditionValue = value,
+        conditionModifier = modifier
+            .size(100.dp)
+            .background(Color.Red)
+            .ifNull(value) { background(Color.Green) },
+    )
+}
+
+@Composable
+fun IfNotNullTestComposable(
+    value: Any?,
+    modifier: Modifier = Modifier,
+) {
+    ConditionTestComposable(
+        conditionName = "ifNotNull",
+        conditionValue = value,
+        conditionModifier = modifier
+            .size(100.dp)
+            .background(Color.Red)
+            .ifNotNull(value) { background(Color.Green) },
+    )
+}
+
+@SuppressLint("ModifierParameter, ComposeModifierWithoutDefault")
+@Composable
+private fun ConditionTestComposable(
+    conditionName: String,
+    conditionValue: Any?,
+    conditionModifier: Modifier,
+) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Condition: $conditionName",
+            style = MaterialTheme.typography.bodyMedium,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(
+            modifier = conditionModifier,
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = conditionValue.toString(),
+                style = MaterialTheme.typography.bodyMedium,
+            )
         }
     }
 }
